6face1b763b71a86aa6786c202e1ba9fd0ebe1b2
mairup <maimairupnik@gmail.com>
2024-03-27 12:11:21 +0100

    V5.2

--- vaje/vaje05/naloga.c
+++ vaje/vaje05/naloga.c
@@ -18,9 +18,21 @@ int vsota(int *zac, int *kon)
 
 void indeksInKazalec(int *t, int *indeks, int **kazalec)
 {
+
+    // printf("indeks : %d\n", *indeks);
+    // printf("kazalec : %d\n", **kazalec);
     if (*indeks == -1)
     {
+        *indeks = *kazalec - t;
     }
+
+    else
+    {
+        *kazalec = t + *indeks;
+    }
+
+    // printf("indeks : %d\n", *indeks);
+    // printf("kazalec : %d\n", **kazalec);
 }
 
 void frekvenceCrk(char *niz, int **frekvence)
@@ -32,14 +44,26 @@ void frekvenceCrk(char *niz, int **frekvence)
 
 int main()
 {
-    int *t = (int *)malloc(10 * sizeof(int));
+    // int *t = (int *)malloc(10 * sizeof(int));
 
-    for (int i = 0; i < 10; i++)
-    {
-        t[i] = 5;
-    }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     t[i] = 5;
+    // }
 
-    printf("vsota : %d\n", vsota(t, t + 9));
+    // printf("vsota : %d\n", vsota(t, t + 9));
+    // t[5] = -1;
+    // int a = -1;
+    // int *b = &a;
+    // int *d = &t[5];
+    // int **c = &d;
+    // indeksInKazalec(t, b, c);
+
+    // printf("\n");
+
+    // a = 4;
+
+    // indeksInKazalec(t, b, c);
 
     return 0;
 }
