d29c58601b152ac2f413ef9ed68168710410906c
mairup <maimairupnik@gmail.com>
2024-04-08 22:20:16 +0200

    n1 - poisci

--- domace_naloge/dn05/naloga1/naloga1.c
+++ domace_naloge/dn05/naloga1/naloga1.c
@@ -13,14 +13,36 @@ gcc -D=test test01.c naloga1.c
 
 #include "naloga1.h"
 
-int* poisci(int* t, int* dolzina, int** konec) {
-    // popravite / dopolnite ...
-    return NULL;
+int *poisci(int *t, int *dolzina, int **konec)
+{
+    int *min = t;
+    int *max = t;
+
+MINSET:
+    if (*min != 0)
+    {
+        min--;
+        goto MINSET;
+    }
+
+MAXSET:
+
+    if (*max != 0)
+    {
+        max++;
+        goto MAXSET;
+    }
+
+    *konec = max - 1;
+    *dolzina = max - min - 1;
+
+    return ++min;
 }
 
 #ifndef test
 
-int main() {
+int main()
+{
     // koda za ro"cno testiranje (po "zelji)
     return 0;
 }
