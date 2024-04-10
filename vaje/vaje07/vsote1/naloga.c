bae39073b7b66c167a96acbfff291856bba94c95
mairup <maimairupnik@gmail.com>
2024-04-10 11:54:04 +0200

    n1 no memo

--- vaje/vaje07/vsote1/naloga.c
+++ vaje/vaje07/vsote1/naloga.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+long nacini(int n, int k)
+{
+    // printf("n: %d, k: %d\n", n, k);
+    if (n == 0 || k == 1)
+    {
+        return 1;
+    }
+    else if (n < 0)
+    {
+        return 0;
+    }
+
+    return nacini(n - k, k) + nacini(n, k - 1);
+}
+
 int main()
 {
     int n;
@@ -7,8 +22,6 @@ int main()
 
     scanf("%d %d", &n, &k);
 
-    for (int i = k; i >= 0; i--)
-    {
-    }
+    printf("%ld\n", nacini(n, k));
     return 0;
 }
