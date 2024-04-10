e07c9a081d7aea3463d2f8a4c678574b477c8e9b
mairup <maimairupnik@gmail.com>
2024-04-10 12:29:38 +0200

    nal1 with memoizacija

--- vaje/vaje07/vsote1/naloga.c
+++ vaje/vaje07/vsote1/naloga.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// /long memo[100000][100];
 long nacini(int n, int k)
 {
     // printf("n: %d, k: %d\n", n, k);
@@ -15,6 +17,25 @@ long nacini(int n, int k)
     return nacini(n - k, k) + nacini(n, k - 1);
 }
 
+long naciniMemo(int n, int k, long **memo)
+{
+    if (n == 0 || k == 1)
+    {
+        return 1;
+    }
+    else if (n < 0 || k < 1)
+    {
+        return 0;
+    }
+    if (memo[n][k] != 0)
+    {
+        return memo[n][k];
+    }
+    // printf("n: %d, k: %d\n", n, k);
+    memo[n][k] = naciniMemo(n - k, k, memo) + naciniMemo(n, k - 1, memo);
+    return memo[n][k];
+}
+
 int main()
 {
     int n;
@@ -22,6 +43,29 @@ int main()
 
     scanf("%d %d", &n, &k);
 
-    printf("%ld\n", nacini(n, k));
+    // printf("%ld\n", nacini(n, k));
+
+    long **memo = malloc((n + 1) * sizeof(long *));
+    for (int i = 0; i <= n; i++)
+    {
+        memo[i] = calloc(k + 1, sizeof(long));
+    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         memo[i][j] = 0;
+    //     }
+    // }
+
+    printf("%ld\n", naciniMemo(n, k, memo));
+
+    for (int i = 0; i < n; i++)
+    {
+        free(memo[i]);
+    }
+    free(memo);
+
+    printf("%ld\n", sizeof(long));
     return 0;
 }
