a18a03bb97570003d6f46f97abc96e07c0fa4092
mairup <maimairupnik@gmail.com>
2024-04-14 16:17:51 +0200

    naloga2 dn07 - prvi del (ap2pp)

--- domace_naloge/dn06/naloga2/naloga2.c
+++ domace_naloge/dn06/naloga2/naloga2.c
@@ -6,20 +6,53 @@
 
 #include "naloga2.h"
 
-int** ap2pp(int(*kazalec)[N], int izvornoStVrstic, int ciljnoStVrstic) {
-    // popravite / dopolnite
-    return NULL;
+int **ap2pp(int (*kazalec)[N], int izvornoStVrstic, int ciljnoStVrstic)
+{
+    int(*tab)[N] = kazalec;
+    int **newT = (int **)malloc(ciljnoStVrstic * sizeof(int *));
+    int vrstLen = ((N * izvornoStVrstic) / ciljnoStVrstic) + 1;
+    int tabI = 0;
+
+    for (int i = 0; i < ciljnoStVrstic; i++)
+    {
+        newT[i] = (int *)malloc(vrstLen * sizeof(int));
+        for (int j = 0; j < vrstLen; j++)
+        {
+            newT[i][j] = (*tab)[tabI];
+            tabI++;
+        }
+    }
+
+    return newT;
 }
 
-int (*pp2ap(int** kazalec, int izvornoStVrstic, int* ciljnoStVrstic))[N] {
+int (*pp2ap(int **kazalec, int izvornoStVrstic, int *ciljnoStVrstic))[N]
+{
     // popravite / dopolnite
     return NULL;
 }
 
 #ifndef test
 
-int main() {
-    // koda za ro"cno testiranje (po "zelji)
+int main()
+{
+    static int tab[][N] = {
+        {5, 3, 2, 1, 7},
+        {33, 8, 2, 9, 3},
+        {4, 5, 5, 21, 5}};
+
+    int **get = ap2pp(tab, 2, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%3d", get[i][j]);
+        }
+        printf("\n");
+        free(get[i]);
+    }
+    free(get);
+
     return 0;
 }
 
