7f2e3d1914e7ae74fbbcac6b8dbd1f3e51362f8b
mairup <maimairupnik@gmail.com>
2024-04-22 23:08:24 +0200

    naloga1 done - very untested

--- domace_naloge/dn07/naloga1/DN07a_63230290.c
+++ domace_naloge/dn07/naloga1/DN07a_63230290.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int preglejOtroke(int **tab, int index)
+{
+    printf("index: %d\n", index);
+    if (tab[index][0] == 0 && tab[index][1] == 0)
+    {
+        return 0;
+    }
+
+    if (tab[index][0] == 0)
+    {
+        return preglejOtroke(tab, index + tab[index][1]) + 1;
+    }
+
+    if (tab[index][1] == 0)
+    {
+        return preglejOtroke(tab, index + tab[index][0]) + 1;
+    }
+
+    int lHeight = preglejOtroke(tab, index + tab[index][0]) + 1;
+    int rHeight = preglejOtroke(tab, index + tab[index][1]) + 1;
+
+    return lHeight > rHeight ? lHeight : rHeight;
+}
+
 int main()
 {
     int n;
     scanf("%d", &n);
-    int **tab = (int *)malloc(n * sizeof(int *));
+    int **tab = (int **)malloc(n * sizeof(int *));
 
     for (int i = 0; i < n; i++)
     {
         tab[i] = (int *)malloc(2 * sizeof(int));
         scanf("%d %d", &tab[i][0], &tab[i][1]);
     }
+
+    printf("%d\n", preglejOtroke(tab, 0));
 
     free(tab);
     return 1;
