04571a6a799aa8d540c5577f1b4349de4312db52
mairup <maimairupnik@gmail.com>
2024-05-05 17:44:30 +0200

    dn08 init

---
+++ domace_naloge/dn08/naloga1/DN08a_63230290.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int tab[10];
+
+int uredi()
+
+    int main()
+{
+    int n, k, r;
+    scanf("%d %d %d", &n, &k, &r);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tab[i]);
+    }
+
+    return 0;
+}
