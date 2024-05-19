2535d5fe747839e455ed07b8b8d91084b4794f38
mairup <maimairupnik@gmail.com>
2024-05-19 23:44:54 +0200

    naloga 2 ni mozna :)

--- domace_naloge/dn10/naloga2/naloga2.c
+++ domace_naloge/dn10/naloga2/naloga2.c
@@ -6,14 +6,23 @@
 
 #include "naloga2.h"
 
-Tocka projekcija(Tocka t, Premica p) {
-    // popravite / dopolnite ...
-    return (Tocka) { (Ulomek) {0, 1}, (Ulomek) {0, 1} };
+Tocka projekcija(Tocka t, Premica p)
+{
+    Ulomek x = (Ulomek){0, 0};
+    Ulomek y = (Ulomek){0, 0};
+
+    x.st = t.y.st / t.y.im + t.x.st / (p.k.st / p.k.im) - p.n.st / p.n.im;
+    x.im = p.k.st / p.k.im + 1 / (p.n.st / p.n.im);
+
+    y.st = t.y.st / t.y.im - t.x.st / (p.n.st / p.n.im) - p.k.st / p.k.im;
+
+    return (Tocka){x, y};
 }
 
 #ifndef test
 
-int main() {
+int main()
+{
     return 0;
 }
 
