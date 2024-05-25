85bcbd98aede494c12146716650a9b6a430bfe33
mairup <maimairupnik@gmail.com>
2024-05-25 22:12:11 +0200

    naloga2 done

--- domace_naloge/dn11/naloga2/naloga2.c
+++ domace_naloge/dn11/naloga2/naloga2.c
@@ -6,14 +6,66 @@
 
 #include "naloga2.h"
 
-int izpisiA(A* a, char* cilj) {
-    // popravite / dopolnite ...
-    return -9999;
+int izpisiA(A *a, char *cilj);
+int izpisiB(B *b, char *cilj);
+
+int izpisiC(C *c, char *cilj)
+{
+    int r = 0;
+    if (c == NULL)
+    {
+        r += sprintf(cilj, "NULL");
+    }
+    else
+    {
+        r += sprintf(cilj, "{%s, ", c->r == true ? "true" : "false");
+        r += izpisiA(c->a, cilj + r);
+        r += sprintf(cilj + r, ", ");
+        r += izpisiB(c->b, cilj + r);
+        r += sprintf(cilj + r, "}");
+    }
+
+    return r;
+}
+
+int izpisiB(B *b, char *cilj)
+{
+    int r = 0;
+    if (b == NULL)
+    {
+        r += sprintf(cilj, "NULL");
+    }
+    else
+    {
+        r += sprintf(cilj, "{%s, ", b->q);
+        r += izpisiC(b->c, cilj + r);
+        r += sprintf(cilj + r, "}");
+    }
+
+    return r;
+}
+
+int izpisiA(A *a, char *cilj)
+{
+    int r = 0;
+    if (a == NULL)
+    {
+        r += sprintf(cilj, "NULL");
+    }
+    else
+    {
+        r += sprintf(cilj, "{%d, ", a->p);
+        r += izpisiB(a->b, cilj + r);
+        r += sprintf(cilj + r, "}");
+    }
+
+    return r;
 }
 
 #ifndef test
 
-int main() {
+int main()
+{
     // koda za ro"cno testiranje (po "zelji)
     return 0;
 }
---
+++ domace_naloge/dn11/naloga2/test01.res
@@ -0,0 +1,2 @@
+{42, {dober, {true, {-15, NULL}, {dan, {false, NULL, NULL}}}}}
+62
