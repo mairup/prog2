13f3625ee07c976c1844996cc5f8ade8e8455106
mairup <maimairupnik@gmail.com>
2024-06-02 17:10:25 +0200

    mid naloga1 dn13

---
+++ domace_naloge/dn13/naloga1/razbij.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *dat = fopen(argv[1], "rb");
+    int k = atoi(argv[2]);
+    FILE* temp;
+    unsigned char byte = fread(&byte, 1, 1, dat);
+    while ()
+    {
+        
+    }
+    return 0;
+}
