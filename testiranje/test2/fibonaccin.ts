bfc08d98e828b4043640dd31765a0d537cba15fb
mairup <maimairupnik@gmail.com>
2024-06-12 20:38:10 +0200

    ts fibonaccin use argumets from console

--- testiranje/test2/fibonaccin.ts
+++ testiranje/test2/fibonaccin.ts
@@ -1,3 +1,5 @@
+declare const Bun: any;
+
 let memo = new Array(1000);
 
 function fibn(n1: number, n2: number, k: number): number {
@@ -15,23 +17,23 @@ function fibn(n1: number, n2: number, k: number): number {
 }
 
 async function main(): Promise<void> {
-  let n1 = 0,
-    n2 = 0,
-    k = 0;
-  let inputVector = new Array(3);
-  let i = 0;
-  for await (const line of console) {
-    inputVector[i] = Number(line);
-    i++;
-    if (i == 3) {
-      n1 = inputVector[0];
-      n2 = inputVector[1];
-      k = inputVector[2];
-      break;
-    }
-  }
+  let n1 = Number(Bun.argv[2]),
+    n2 = Number(Bun.argv[3]),
+    k = Number(Bun.argv[4]);
+  //   let inputVector = new Array(3);
+  //   let i = 0;
+  //   for await (const line of console) {
+  //     inputVector[i] = Number(line);
+  //     i++;
+  //     if (i == 3) {
+  //       n1 = inputVector[0];
+  //       n2 = inputVector[1];
+  //       k = inputVector[2];
+  //       break;
+  //     }
+  //   }
   let x: number;
-  for (i = 0; i <= k; i++) {
+  for (let i = 0; i <= k; i++) {
     x = fibn(n1, n2, i);
     console.log(`fibn(${i}) = [${x}]`);
   }
