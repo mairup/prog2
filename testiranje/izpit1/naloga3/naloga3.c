354ae8721df40495af8434f2b5c2b33100a22cb2
mairup <maimairupnik@gmail.com>
2024-07-04 14:26:36 +0200

    izpit1 nal3 done

--- testiranje/izpit1/naloga3/naloga3.c
+++ testiranje/izpit1/naloga3/naloga3.c
@@ -10,11 +10,47 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+static int zapPoteza = 1;
+static int littlePos = 'A';
 
 // po potrebi dopolnite ...
 
-int main() {
-    // dopolnite ...
+void hanoi(int n, char a, char b, char c, int k)
+{
 
+        if (n == 1)
+    {
+        if (zapPoteza > k && littlePos == a && c == 'B')
+        {
+            printf("%d\n", zapPoteza);
+        }
+        zapPoteza++;
+        // printf("%c -> %c,", a, c);
+        if (a == littlePos)
+        {
+            littlePos = c;
+        }
+        return;
+    }
+
+    hanoi(n - 1, a, c, b, k);
+    if (zapPoteza > k && littlePos == a && c == 'B')
+    {
+        printf("%d\n", zapPoteza);
+    }
+    zapPoteza++;
+    if (a == littlePos)
+    {
+        littlePos = c;
+    }
+    // printf("%c -> %c,", a, c);
+    hanoi(n - 1, b, a, c, k);
+}
+
+int main()
+{
+    int n, k;
+    scanf("%d%d", &n, &k);
+    hanoi(n, 'A', 'B', 'C', k);
     return 0;
 }
